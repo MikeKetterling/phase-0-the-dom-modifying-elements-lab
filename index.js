6cf9e599024f8ac7def437842485ca643689461b
Mike <mikekett4@yahoo.com>
2021-09-30 13:54:05 -0600

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.id = "victory";
+newHeader.innerHTML = "Mike is the champion"
